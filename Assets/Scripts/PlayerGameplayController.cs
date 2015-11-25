63b97c982fb58682b287a10db1f162ea06a36c7e
Martin Zielinski <darthcoder1@gmail.com>
2015-11-25 23:04:40 +0100

    assigned rig-controller to player prefab

--- Assets/Scripts/PlayerGameplayController.cs
+++ Assets/Scripts/PlayerGameplayController.cs
@@ -6,18 +6,24 @@ public class PlayerGameplayController : MonoBehaviour {
 	public bool isShooting;
 
 	private SMF.RubberBandController inputComp;
+	private Animator animComp;
 
 	// Use this for initialization
 	void Start () 
 	{
 		inputComp = GetComponent<SMF.RubberBandController>();
+		animComp = GetComponent<Animator>();
+
 		Debug.Assert(inputComp);
+		Debug.Assert(animComp);
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
 		isShooting = inputComp.numActiveTouches > 0;
+
+		animComp.SetFloat("X_Input", inputComp.currentVelocity.x);
 	}
 
 	void OnTriggerEnter2D(Collider2D other)
